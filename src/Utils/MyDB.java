f389b9dc75dbace6fcd9f1d0649560ac5fd8fd0b
BSnassim <nassim.bensalah@esprit.tn>
2023-02-12 15:37:18 +0100

    test7

--- src/Utils/MyDB.java
+++ src/Utils/MyDB.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyDB {
-    final String url = "jdbc:mysql://localhost:3306/treydi_db";
+    final String url = "jdbc:mysql://localhost:3306/tr";
     final String user = "root";
     final String password = "";
     static MyDB instance;
